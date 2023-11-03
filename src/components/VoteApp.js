688ac9686576b50aba585141fb7beab9a4bce96f
Aj7Ay <110721907+Aj7Ay@users.noreply.github.com>
2023-11-03 20:14:55 +0530

    Update VoteApp.js

--- src/components/VoteApp.js
+++ src/components/VoteApp.js
@@ -8,7 +8,7 @@ class VoteApp extends Component {
         <div class="jumbotron">
           <div class="container">
             <h1 class="display-3">Language Vote App v1</h1>
-            &copy; CloudAcademy ❤ DevOps 2019
+            &copy; Mrcloudbook ❤ DevOps 2029
           </div>
         </div>
 
